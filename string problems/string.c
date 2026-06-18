3d8293bdce4957b0b00dc8cb822cc6b87730d3e7
mukherjeesoumadip93-stack <mukherjeesoumadip93@gmail.com>
2026-06-18 17:29:29 +0530

    Update string.c

--- string problems/string.c
+++ string problems/string.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<string.h>
 
+int main (){          //sorting the names of students in class
+char stu[][20]={
+    "soumo",
+    "bag",
+    "sreejoyee",
+    "nafis"
+    
+};
+int i,j;
+char temp[50];
+for(i=0;i<3;i++){
+    for(j=1;j<4;j++){
+       if (strcmp(stu[i],stu[j])>0)
+        {
+        strcpy(temp,stu[i]);
+        strcpy(stu[i],stu[j]);
+        strcpy(stu[j],temp);
+       }                           //strcmp-for comparison,//strcpy-for string copy
+    }
+}
+printf("sorted names:\n");
+
+for(int i=0;i<4;i++){
+    printf("%s\n",stu[i]);
+}
+}
