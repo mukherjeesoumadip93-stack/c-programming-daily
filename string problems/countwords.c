6120494338393b3a2687e1f0d30f7155fb8a549c
The C Explorer <mukherjeesoumadip93@gmail.com>
2026-06-21 17:43:39 +0530

    Update countwords.c

--- string problems/countwords.c
+++ string problems/countwords.c
@@ -1,3 +1,10 @@
+/*
+Program: counting words in a sentence
+Concepts: Strings, Loops
+Difficulty: Beginner
+Author: Soumadip Mukherjee
+*/
+
 #include<stdio.h>
 #include<string.h> 
 #include<ctype.h>                
@@ -14,10 +21,10 @@ int main(){
    char *str=string;
     while(*str){
         if(isspace(*str)){
-            word=0;
+            word=0;  //when we hit space
         }
         else if(!word){
-            word=1;
+            word=1;  //words after hitting spaces
             count++;
         }
         str++;
