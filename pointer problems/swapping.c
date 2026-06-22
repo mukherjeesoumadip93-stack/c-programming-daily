789b1295dfda1c7f47c29481efe9dfda5a958549
The C Explorer <mukherjeesoumadip93@gmail.com>
2026-06-22 08:28:17 +0530

    swap function for two integers
    a program for swapping two number using only their memory address

---
+++ pointer problems/swapping.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+    printf("after swapping in function:%d %d\n",*a,*b);
+}
+
+int main(){
+    int x=10;
+    int y=12;
+    
+    swap(&x,&y);               //call by  address
+
+    printf("the values after swapping in main:%d %d",x,y);
+}
